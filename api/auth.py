23d7098f42804d380a44a2c7023784f9e6593e8c
Wilson Angelie Tan <wilsonangelietann@gmail.com>
2025-08-20 10:15:13 +0700

    fix(api): update HTTP exception status code

--- api/auth.py
+++ api/auth.py
@@ -9,7 +9,7 @@ router = APIRouter()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # register
-@router.post("/register")
+@router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register(user: UserRegister):
     hashed_pw = pwd_context.hash(user.password)
     is_success, msg = await insert_user(user.username, user.email, hashed_pw)
@@ -19,11 +19,11 @@ async def register(user: UserRegister):
             
 
 # login
-@router.post("/login", response_model=Token)
+@router.post("/login", response_model=Token, status_code=status.HTTP_200_OK)
 async def login(user: UserLogin):
     dbuser, msg = await get_user_by_username_or_email(user.username_or_email)
     if not dbuser or not pwd_context.verify(user.password, dbuser.password):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username or Password is wrong")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Username or Password")
     
     access_token = create_access_token(
         data={"sub": dbuser.id, "username": dbuser.username},
--- api/classes.py
+++ api/classes.py
@@ -2,58 +2,63 @@ import uuid
 from fastapi import Depends, APIRouter, HTTPException, status
 from utils.auth import get_current_user
 from utils.database import get_all_classes, get_classes_by_id, insert_classes, update_classes_by_id, delete_classes_by_id
+from utils.helper import error_exception
 from api_schemas.classes import AddClass, EditClass
 
 router = APIRouter()
 
-# liat daftar class yang tersedia
+
+# lihat daftar kelas yang tersedia
 @router.get("/", status_code=status.HTTP_200_OK)
 async def get_all_class(current_user=Depends(get_current_user)):
     classes, msg = await get_all_classes()
-    if not classes:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=msg)
+    error_exception(msg)
+    
     return { "data": classes, "message": msg }
 
 
-# cek detail class
-@router.get("/{id}", status_code=status.HTTP_200_OK)
-async def get_class_by_id(id: uuid.UUID, current_user=Depends(get_current_user)):
-    classes, msg = await get_classes_by_id(id)
-    if not classes:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=msg)
+# cek detail kelas
+@router.get("/{class_id}", status_code=status.HTTP_200_OK)
+async def get_class_by_id(class_id: uuid.UUID, current_user=Depends(get_current_user)):
+    classes, msg = await get_classes_by_id(class_id)
+    error_exception(msg)
+    
     return { "data": classes, "message": msg }
 
 
-# tambah class
+# tambah kelas - admin privilege only (akun admin tersedia dari seeding database)
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def add_class(classes: AddClass ,current_user=Depends(get_current_user)):
     if current_user.level != "admin":
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not Authorized")
     
-    is_success, msg = insert_classes(AddClass.name, AddClass.detail)
+    is_success, msg = await insert_classes(classes.name, classes.detail)
     if not is_success:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg)
+        error_exception(msg)
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT)
     
-    return {}
+    return { "message": msg }
 
 
-# edit class - admin privilege only (akun admin tersedia dari seeding database)
-@router.put("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def edit_class(id: uuid.UUID, classes: EditClass, current_user=Depends(get_current_user)):
+# edit kelas - admin privilege only 
+@router.put("/{class_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def edit_class(class_id: uuid.UUID, classes: EditClass, current_user=Depends(get_current_user)):
     if current_user.level != "admin":
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not Authorized")
     
-    is_success, msg = await update_classes_by_id(id=id, name=EditClass.name, detail=EditClass.detail)
+    is_success, msg = await update_classes_by_id(id=class_id, name=classes.name, detail=classes.detail)
     if not is_success:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg)
+        error_exception(msg)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=msg)
 
 
 # hapus class - admin privilege only
-@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_class(id: uuid.UUID, current_user=Depends(get_current_user)):
+@router.delete("/{class_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_class(class_id: uuid.UUID, current_user=Depends(get_current_user)):
     if current_user.level != "admin":
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not Authorized")
 
-    is_success, msg = await delete_classes_by_id(id)
+    is_success, msg = await delete_classes_by_id(class_id)
     if not is_success:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg)
+        error_exception(msg)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=msg)
