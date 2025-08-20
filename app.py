cdf87ce967462d4b561b07e7e174bc82d73e8e6b
Wilson Angelie Tan <wilsonangelietann@gmail.com>
2025-08-20 10:14:07 +0700

    feat: add route '/'

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import os
 import asyncio
 import uvicorn
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, APIRouter, status
+from fastapi.responses import RedirectResponse
 from dotenv import load_dotenv
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from api import auth, classes, enrollment
 
 app = FastAPI()
+router = APIRouter()
 security = HTTPBearer()
 
+
+@router.get("/")
+async def root():
+    return RedirectResponse(url="/docs", status_code=status.HTTP_302_FOUND)
+
+app.include_router(router, tags=["Redirect to docs"])
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(classes.router, prefix="/class", tags=["class"])
 app.include_router(enrollment.router, prefix="/enroll", tags=["enrollment"])
-
-async def main():
-    config = uvicorn.Config("app:app", host="0.0.0.0", port=8000, reload=True)
-    server = uvicorn.Server(config)
-    await server.serve()
-
-if __name__ == "__main__":
-   asyncio.run(main())
